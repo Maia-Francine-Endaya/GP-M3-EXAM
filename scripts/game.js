0672181c026a3f77bda410bd304582de1dfceb8d
Maia Francine Endaya <maia.endaya@gmail.com>
2022-05-03 09:18:26 +0800

    added_player_movement_and_coins

--- scripts/game.js
+++ scripts/game.js
@@ -18,18 +18,24 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+var platforms;
+var player;
+var coins;
+var cursors;
+
 function preload() {
   this.load.image('background', './assets/images/Background.png');
-  this.load.image('thief', './assets/images/Thief.PNG');
   this.load.image('ground', './assets/images/Stone_Ground.PNG');
   this.load.image('bigCoin', './assets/images/Gold_Coin.PNG');
   this.load.image('midCoin', './assets/images/Silver_Coin.PNG');
   this.load.image('smallCoin', './assets/images/Bronze_Coin.PNG');
   this.load.image('merchant', './assets/images/Merchant.PNG');
+  this.load.spritesheet('thief', './assets/images/Thief.PNG', { frameWidth: 32, frameHeight: 38 });
 };
 
 function create() {
 
+  //Platforms
   platforms = this.physics.add.staticGroup();
 
   platforms.create(100, 780, 'ground');
@@ -41,122 +47,74 @@ function create() {
   platforms.create(1600, 780, 'ground');
   platforms.create(1850, 780, 'ground');
 
-  //Player TO DO: TWEAK THE CODE
+  //Coins
+  coins = this.physics.add.staticGroup();
+
+  coins.create(250, 743, 'bigCoin');
+  coins.create(370, 747, 'smallCoin');
+  coins.create(567, 745, 'midCoin');
+  coins.create(621, 747, 'smallCoin');
+  coins.create(675, 747, 'smallCoin');
+  coins.create(812, 743, 'bigCoin');
+  coins.create(903, 745, 'midCoin');
+
+  //Player
   player = this.physics.add.sprite(100, 450, 'thief');
 
   player.setBounce(0.2);
-  player.setCollideWorldBounds(false);
-  player.body.setGravityY(290);
+  player.setCollideWorldBounds(true);
 
-  //Player Animation
   this.anims.create({
     key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frames: this.anims.generateFrameNumbers('thief', { start: 4, end: 7 }),
     frameRate: 10,
     repeat: -1
   });
 
   this.anims.create({
-    key: 'turn',
-    frames: [{ key: 'dude', frame: 4 }],
+    key: 'idle',
+    frames: [{ key: 'thief', frame: 1 }],
+    frameRate: 20
   });
 
   this.anims.create({
     key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frames: this.anims.generateFrameNumbers('thief', { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1
   });
-
-  //Coins
-  coins = this.physics.add.group({
-    key: 'coin',
-    repeat: 13,
-    setXY: { x: 100, y: 0, stepX: 130 }
-  });
-
-  coins.children.iterate(function (child) {
-    child.setBounceY(Phaser.Math.FloatBetween(0.2, 0.4));
-  });
-
-  //Goal
-  goal = this.physics.add.staticGroup();
-  goal.create(40, 220, 'goal');
-
 
   //Cursors
   cursors = this.input.keyboard.createCursorKeys();
 
   //Physics
   this.physics.add.collider(player, platforms);
-  this.physics.add.collider(coins, platforms);
-
   this.physics.add.overlap(player, coins, collectCoin, null, this);
-  this.physics.add.overlap(player, goal, winFunction, null, this);
-  this.physics.add.overlap(player, hazards, loseLives, null, this);
-  ;
+};
 
-  function update() {
-    //Player Movement
-    if (cursors.left.isDown) {
-      player.setVelocityX(-160);
-      player.anims.play('left', true);
-    }
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
 
-    else if (cursors.right.isDown) {
-      player.setVelocityX(160);
-      player.anims.play('right', true);
-    }
+    player.anims.play('left', true);
+  }
+  else if (cursors.right.isDown) {
+    player.setVelocityX(160);
 
-    else {
-      player.setVelocityX(0);
-      player.anims.play('turn');
-    }
+    player.anims.play('right', true);
+  }
+  else {
+    player.setVelocityX(0);
 
-    if (cursors.up.isDown && player.body.touching.down) {
-      player.setVelocityY(-330);
-    }
+    player.anims.play('idle');
   }
 
-  //Function of adding score and number of coins collected
-  function collectCoin(player, coins) {
-
-    coins.disableBody(true, true);
-
-    score += 10;
-    coinsCollected += 1;
-
-    scoreText.setText('Score: ' + score);
-    collectedText.setText('Coins Collected: ' + coinsCollected);
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
   }
 
-  //Function of ending the game when goal is reached
-  function winFunction(player, goal) {
-    this.physics.pause();
+};
 
-    goal.disableBody(true, true);
-
-    //Win Text
-    winText = this.add.text(600, 300, 'YOU WIN!', { fontSize: '100px', fill: '#000' });
-
-    player.setTint(0xffb608);
-    player.anims.play('turn');
-    win = true;
-  }
-}
-
-//Function of subtracting lives as player collides with hazards
-function loseLives(player, hazards) {
-
-  lives -= 1;
-  livesCounter.setText('Lives Left: ' + lives)
-
-  if (lives == 0) {
-    this.physics.pause();
-    player.setTint(0xbd0000);
-    player.anims.play('turn');
-
-    gameOverText = this.add.text(600, 300, 'YOU LOSE!', { fontSize: '100px', fill: '#000' })
-    gameOver = true;
-  }
+function collectCoin(player, coins) {
+  coins.disableBody(true, true);
 }
