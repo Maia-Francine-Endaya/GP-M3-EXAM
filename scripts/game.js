d3dcc3621f798704aedb13bfc7d79802e85de038
Maia Francine Endaya <maia.endaya@gmail.com>
2022-05-03 09:51:09 +0800

    added_scoring_and_score_goal_system

--- scripts/game.js
+++ scripts/game.js
@@ -23,8 +23,14 @@ var player;
 var coins;
 var cursors;
 
+var score = 0;
+var scoreText;
+
+var scoreGoal = 175;
+var scoreGoalText;
+
 function preload() {
-  this.load.image('background', './assets/images/Background.png');
+  this.load.image('background', './assets/images/Placeholder_background.jpeg');
   this.load.image('ground', './assets/images/Stone_Ground.PNG');
   this.load.image('bigCoin', './assets/images/Gold_Coin.PNG');
   this.load.image('midCoin', './assets/images/Silver_Coin.PNG');
@@ -34,10 +40,18 @@ function preload() {
 };
 
 function create() {
+  this.add.image(900, 290, 'background');
+
+  //Displays the score the player has
+  scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+
+  //Displays the score goal
+  scoreGoalText = this.add.text(250, 16, 'Score Goal:' + scoreGoal, { fontSize: '32px', fill: '#000' });
 
   //Platforms
   platforms = this.physics.add.staticGroup();
 
+  //Places the platforms
   platforms.create(100, 780, 'ground');
   platforms.create(350, 780, 'ground');
   platforms.create(600, 780, 'ground');
@@ -48,15 +62,24 @@ function create() {
   platforms.create(1850, 780, 'ground');
 
   //Coins
-  coins = this.physics.add.staticGroup();
+  //Separated into three groups as each give different scores
+  smallCoins = this.physics.add.staticGroup();
 
-  coins.create(250, 743, 'bigCoin');
-  coins.create(370, 747, 'smallCoin');
-  coins.create(567, 745, 'midCoin');
-  coins.create(621, 747, 'smallCoin');
-  coins.create(675, 747, 'smallCoin');
-  coins.create(812, 743, 'bigCoin');
-  coins.create(903, 745, 'midCoin');
+  smallCoins.create(370, 747, 'smallCoin');
+  smallCoins.create(621, 747, 'smallCoin');
+  smallCoins.create(675, 747, 'smallCoin');
+
+
+  midCoins = this.physics.add.staticGroup();
+
+  midCoins.create(567, 745, 'midCoin');
+  midCoins.create(903, 745, 'midCoin');
+
+
+  bigCoins = this.physics.add.staticGroup();
+
+  bigCoins.create(250, 743, 'bigCoin');
+  bigCoins.create(812, 743, 'bigCoin');
 
   //Player
   player = this.physics.add.sprite(100, 450, 'thief');
@@ -89,7 +112,9 @@ function create() {
 
   //Physics
   this.physics.add.collider(player, platforms);
-  this.physics.add.overlap(player, coins, collectCoin, null, this);
+  this.physics.add.overlap(player, smallCoins, smallScore, null, this);
+  this.physics.add.overlap(player, midCoins, midScore, null, this);
+  this.physics.add.overlap(player, bigCoins, bigScore, null, this);
 };
 
 function update() {
@@ -115,6 +140,32 @@ function update() {
 
 };
 
-function collectCoin(player, coins) {
-  coins.disableBody(true, true);
-}
+//Functions for adding score according to the coin collected
+
+//Big coin = 50
+//Medium coin = 25
+//Small coin = 12
+
+function smallScore(player, smallCoins) {
+  smallCoins.disableBody(true, true);
+
+  score += 12;
+  scoreText.setText('Score: ' + score);
+
+};
+
+function midScore(player, midCoins) {
+  midCoins.disableBody(true, true);
+
+  score += 25;
+  scoreText.setText('Score: ' + score);
+
+};
+
+function bigScore(player, bigCoins) {
+  bigCoins.disableBody(true, true);
+
+  score += 50;
+  scoreText.setText('Score: ' + score);
+
+};
